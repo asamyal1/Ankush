8589fffdb7147b0e7171c45caba24952471cd0d4
asamyal1 <ankusheica@gmail.com>
2019-01-09 13:22:34 +0530

    my test php files

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'php';
+$databaseUsername = 'root';
+$databasePassword = 'root';
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+//including the database connection file
+include_once("config.php");
+ 
+//fetching data in descending order (lastest entry first)
+//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
+?>
+ 
+<html>
+<head>    
+    <title>Homepage</title>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+ 
+<body>
+<div class="container">
+    <a href="add.html">Add New Data</a><br/><br/>
+ 
+    <table class="table table-hover table-bordered">
+        <tr bgcolor='#CCCCCC'>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Email</td>
+            <td>Update</td>
+        </tr>
+        <?php 
+        //while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
+        while($res = mysqli_fetch_array($result)) {         
+            echo "<tr>";
+            echo "<td>".$res['name']."</td>";
+            echo "<td>".$res['age']."</td>";
+            echo "<td>".$res['email']."</td>";    
+            echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";        
+        }
+        ?>
+    </table>
+  <?php
+$x = 100;  
+$y = 50;
+
+if ($x == 100 || $y == 50) {
+    echo "Hello world!";
+}
+echo '<br>';
+
+
+$i = 1; 
+
+
+do
+ {
+    echo $i;
+    $i++;
+} 
+while
+ ($i < 6);
+
+
+?>
+
+<?php
+$myfile = fopen("testfile.txt", "w");
+
+$myfile = fopen("testfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
+</div>
+</body>
+</html>
